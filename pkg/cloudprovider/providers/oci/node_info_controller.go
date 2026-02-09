cb8df43b185cd1df30772be94544f6fbd68cb59e
Pascal Reeb <pascal@reeb.io>
2026-02-09 22:09:38 +0000

    feat(node_controller): Set ProviderID
    Signed-off-by: Pascal Reeb <pascal@reeb.io>

--- pkg/cloudprovider/providers/oci/node_info_controller.go
+++ pkg/cloudprovider/providers/oci/node_info_controller.go
@@ -149,7 +149,7 @@ func (nic *NodeInfoController) processNextItem() bool {
 	return true
 }
 
-// A function which is responsible for adding the fault domain label and CompartmentID annotation to the node if it
+// A function which is responsible for setting the ProviderID, adding the fault domain label and CompartmentID annotation to the node if it
 // is not already present. Also cache the instance information
 func (nic *NodeInfoController) processItem(key string) error {
 
@@ -161,13 +161,13 @@ func (nic *NodeInfoController) processItem(key string) error {
 		return err
 	}
 
-	// if node has required labels already, don't process agin
+	// if node has required labels already, don't process again
 	if validateNodeHasRequiredLabels(cacheNode) {
-		logger.With("nodeName", cacheNode.Name).Debugf("The node has the fault domain label and compartmentID annotation already, will not process")
+		logger.With("nodeName", cacheNode.Name).Debugf("The node has the ProviderID, fault domain label and compartmentID annotation already, will not process")
 		return nil
 	}
 
-	instance, err := getInstanceByNode(cacheNode, nic, logger)
+	instance, providerID, err := getInstanceByNode(cacheNode, nic, logger)
 	if err != nil {
 		return err
 	}
@@ -177,7 +177,7 @@ func (nic *NodeInfoController) processItem(key string) error {
 		return err
 	}
 
-	nodePatchBytes := getNodePatchBytes(cacheNode, instance, logger)
+	nodePatchBytes := getNodePatchBytes(cacheNode, instance, providerID, logger)
 
 	if nodePatchBytes == nil {
 		return nil
@@ -195,7 +195,7 @@ func (nic *NodeInfoController) processItem(key string) error {
 	return nil
 }
 
-func getNodePatchBytes(cacheNode *v1.Node, instance *core.Instance, logger *zap.SugaredLogger) []byte {
+func getNodePatchBytes(cacheNode *v1.Node, instance *core.Instance, providerID string, logger *zap.SugaredLogger) []byte {
 	if validateNodeHasRequiredLabels(cacheNode) {
 		return nil
 	}
@@ -207,71 +207,129 @@ func getNodePatchBytes(cacheNode *v1.Node, instance *core.Instance, logger *zap.
 	availabilityDomainLabelValue := strings.ReplaceAll(*instance.AvailabilityDomain, ":", ".")
 
 	var nodePatchBytes []byte
+
+	// Helper function to build patch with ProviderID
+	buildPatchWithProviderID := func(metadataJSON string) string {
+		return fmt.Sprintf("{\"spec\": {\"providerID\": \"%s\"}, \"metadata\": %s}", providerID, metadataJSON)
+	}
+
+	// Helper function to build metadata JSON for labels and annotations
+	buildMetadata := func(labels map[string]string, annotations map[string]string) string {
+		hasLabels := len(labels) > 0
+		hasAnnotations := len(annotations) > 0
+
+		if !hasLabels && !hasAnnotations {
+			return "{}"
+		}
+
+		var jsonParts []string
+		if hasLabels {
+			labelJSON := "{"
+			first := true
+			for k, v := range labels {
+				if !first {
+					labelJSON += ","
+				}
+				labelJSON += fmt.Sprintf("\"%s\":\"%s\"", k, v)
+				first = false
+			}
+			labelJSON += "}"
+			jsonParts = append(jsonParts, fmt.Sprintf("\"labels\": %s", labelJSON))
+		}
+
+		if hasAnnotations {
+			annJSON := "{"
+			first := true
+			for k, v := range annotations {
+				if !first {
+					annJSON += ","
+				}
+				annJSON += fmt.Sprintf("\"%s\":\"%s\"", k, v)
+				first = false
+			}
+			annJSON += "}"
+			jsonParts = append(jsonParts, fmt.Sprintf("\"annotations\": %s", annJSON))
+		}
+
+		return "{" + strings.Join(jsonParts, ",") + "}"
+	}
+
 	if isFaultDomainLabelPresent && (!client.IsIpv6SingleStackCluster() || isAvailabilityDomainLabelPresent) {
 		//In this case CompartmentIDAnnotation not present but FaultDomainLabel present
+		logger.Infof("Adding ProviderID from cloud provider: %s", providerID)
 		logger.Infof("Adding node annotation from cloud provider: %s=%s", CompartmentIDAnnotation, *instance.CompartmentId)
-		nodePatchBytes = []byte(fmt.Sprintf("{\"metadata\": {\"annotations\": {\"%s\":\"%s\"}}}",
-			CompartmentIDAnnotation, *instance.CompartmentId))
+
+		annotations := map[string]string{CompartmentIDAnnotation: *instance.CompartmentId}
+		metadataJSON := buildMetadata(nil, annotations)
+		nodePatchBytes = []byte(buildPatchWithProviderID(metadataJSON))
 	} else if isCompartmentIDAnnotationPresent {
 		//In this case FaultDomainLabel not present but CompartmentIDAnnotation present
+		logger.Infof("Adding ProviderID from cloud provider: %s", providerID)
 		logger.Infof("Adding node label from cloud provider: %s=%s", FaultDomainLabel, *instance.FaultDomain)
+
+		labels := map[string]string{FaultDomainLabel: *instance.FaultDomain}
 		if client.IsIpv6SingleStackCluster() {
 			logger.Infof("Adding node label from cloud provider: %s=%s", AvailabilityDomainLabel, availabilityDomainLabelValue)
-			nodePatchBytes = []byte(fmt.Sprintf("{\"metadata\": {\"labels\": {\"%s\":\"%s\",\"%s\":\"%s\"}}}", FaultDomainLabel, *instance.FaultDomain,
-				AvailabilityDomainLabel, availabilityDomainLabelValue))
-		} else {
-			nodePatchBytes = []byte(fmt.Sprintf("{\"metadata\": {\"labels\": {\"%s\":\"%s\"}}}", FaultDomainLabel, *instance.FaultDomain))
+			labels[AvailabilityDomainLabel] = availabilityDomainLabelValue
 		}
 
+		metadataJSON := buildMetadata(labels, nil)
+		nodePatchBytes = []byte(buildPatchWithProviderID(metadataJSON))
 	} else {
 		//In this case none of FaultDomainLabel or CompartmentIDAnnotation present
+		logger.Infof("Adding ProviderID from cloud provider: %s", providerID)
 		logger.Infof("Adding node label from cloud provider: %s=%s", FaultDomainLabel, *instance.FaultDomain)
 		logger.Infof("Adding node annotation from cloud provider: %s=%s", CompartmentIDAnnotation, *instance.CompartmentId)
 
+		labels := map[string]string{FaultDomainLabel: *instance.FaultDomain}
+		annotations := map[string]string{CompartmentIDAnnotation: *instance.CompartmentId}
+
 		if client.IsIpv6SingleStackCluster() {
 			logger.Infof("Adding node label from cloud provider: %s=%s", AvailabilityDomainLabel, availabilityDomainLabelValue)
-			nodePatchBytes = []byte(fmt.Sprintf("{\"metadata\": {\"labels\": {\"%s\":\"%s\",\"%s\":\"%s\"},\"annotations\": {\"%s\":\"%s\"}}}",
-				FaultDomainLabel, *instance.FaultDomain, AvailabilityDomainLabel, availabilityDomainLabelValue, CompartmentIDAnnotation, *instance.CompartmentId))
-		} else {
-			nodePatchBytes = []byte(fmt.Sprintf("{\"metadata\": {\"labels\": {\"%s\":\"%s\"},\"annotations\": {\"%s\":\"%s\"}}}",
-				FaultDomainLabel, *instance.FaultDomain, CompartmentIDAnnotation, *instance.CompartmentId))
+			labels[AvailabilityDomainLabel] = availabilityDomainLabelValue
 		}
+
+		metadataJSON := buildMetadata(labels, annotations)
+		nodePatchBytes = []byte(buildPatchWithProviderID(metadataJSON))
 	}
+
 	return nodePatchBytes
 }
 
-func getInstanceByNode(cacheNode *v1.Node, nic *NodeInfoController, logger *zap.SugaredLogger) (*core.Instance, error) {
+func getInstanceByNode(cacheNode *v1.Node, nic *NodeInfoController, logger *zap.SugaredLogger) (*core.Instance, string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
 	providerID := cacheNode.Spec.ProviderID
 	var err error
 	if providerID == "" {
-		providerID, err = nic.cloud.InstanceID(ctx, types.NodeName(cacheNode.Name))
+		instanceID, err := nic.cloud.InstanceID(ctx, types.NodeName(cacheNode.Name))
 		if err != nil {
-			logger.With(zap.Error(err)).Error("Failed to map provider ID to instance ID")
-			return nil, err
+			logger.With(zap.Error(err)).Error("Failed to get instance ID")
+			return nil, "", err
 		}
+		providerID = providerPrefix + instanceID
 	}
 
 	instanceID, err := MapProviderIDToResourceID(providerID)
 	if err != nil {
 		logger.With(zap.Error(err)).Error("Failed to map providerID to instanceID")
-		return nil, err
+		return nil, "", err
 	}
 	instance, err := nic.ociClient.Compute().GetInstance(ctx, instanceID)
 	if err != nil {
 		logger.With(zap.Error(err)).Error("Failed to get instance from instance ID")
-		return nil, err
+		return nil, "", err
 	}
-	return instance, nil
+	return instance, providerID, nil
 }
 
 func validateNodeHasRequiredLabels(node *v1.Node) bool {
+	isProviderIDSet := node.Spec.ProviderID != ""
 	_, isFaultDomainLabelPresent := node.ObjectMeta.Labels[FaultDomainLabel]
 	_, isAvilabilityDomainNameLabelPresent := node.ObjectMeta.Labels[AvailabilityDomainLabel]
 	_, isCompartmentIDAnnotationPresent := node.ObjectMeta.Annotations[CompartmentIDAnnotation]
-	if isFaultDomainLabelPresent && isCompartmentIDAnnotationPresent && (!client.IsIpv6SingleStackCluster() || isAvilabilityDomainNameLabelPresent) {
+	if isProviderIDSet && isFaultDomainLabelPresent && isCompartmentIDAnnotationPresent && (!client.IsIpv6SingleStackCluster() || isAvilabilityDomainNameLabelPresent) {
 		return true
 	}
 	return false
